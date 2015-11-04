711ecca37c264f282437652945972d7215e5c49d
Zhenxin Zhang <zhenxin.zhang@outlook.com>
2015-11-05 05:41:45 +0800

    simplify the contents to suit the web design

--- register.php
+++ register.php
@@ -10,51 +10,44 @@ require('config.php');
 
 if(isset($_POST['submit'])){
 //Perform the verification
-    $email1 = $_POST['email1'];
-    $email2 = $_POST['email2'];
-    $pass1 = $_POST['pass1'];
-    $pass2 = $_POST['pass2'];
+    $uname = $_POST['uname'];
 
-    if($email1 == $email2){
-        if($pass1 == $pass2){
+    $pass = $_POST['pass'];
+
+    if(0){
+        if(0){
             //Verified.
-            $name=mysql_escape_string($_POST['name']);
-            $lname=mysql_escape_string($_POST['lname']);
-            $uname=mysql_escape_string($_POST['uname']);
-            $email1=mysql_escape_string($email1);
-            $email2=mysql_escape_string($email2);
-            $pass1=mysql_escape_string($pass1);
-            $pass2=mysql_escape_string($pass2);
 
-            $pass1=md5($pass1);
+
+            $uname=mysql_escape_string($_POST['uname']);
+            $pass=mysql_escape_string($pass);
+            $pass=md5($pass);
 
             $sql=mysql_query("SELECT * FROM `users` WHERE `uname`='$uname'");
             if(mysql_num_rows($sql)>0){
                 echo "Sorry, that user already exists.";
                 exit();
             }
-            mysql_query("INSERT INTO `users` (`id`,`name`,`lname`,`uname`,`email`,`pass`) VALUES (NULL,'$name' ,'$lname','$uname','$email1','$pass1')");
+            mysql_query("INSERT INTO `users` (`id`,`uname`,`pass`) VALUES (NULL,'$uname','$pass')");
 
 
         }else{
             echo "Sorry, your passwords do not match. <br />";
             exit();
+            //Just in case you need to double check user input
         }
     }else{
         echo "Sorry, your emails do not match. <br />";
+        exit();
+        //Just in case you need to double check user input
     }
 
 
 }else{
     $form = <<<EOT
 <form action="register.php" method="POST">
-First Name:<input type="text" name="name" /><br />
-Last Name:<input type="text" name="lname" /><br />
 Username:<input type="text" name="uname" /><br />
-Email:<input type="text" name="email1" /><br />
-Confirm Email:<input type="text" name="email2" /><br />
 Password:<input type="password" name="pass1" /><br />
-Confirm Password:<input type="password" name="pass2" /><br />
 <input type="submit" value="Register" name="submit" />
 
 </form>
