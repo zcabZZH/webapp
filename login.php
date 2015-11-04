fc608f22dc5e2cdbd7d4d2ac330ca62ec6c45006
Zhenxin Zhang <zhenxin.zhang@outlook.com>
2015-11-05 05:35:03 +0800

    Develop login page

--- login.php
+++ login.php
@@ -6,6 +6,32 @@
  * Time: 13:08
  */
 
+require('config.php');
+if(isset($_POST['submit'])) {
+    $uname = mysql_escape_string($_POST['uname']);
+    $pass = mysql_escape_string($_POST['pass']);
+    $pass = md5($pass);
+
+    $sql = mysql_query("SELECT * FROM `users` WHERE `uname` = '$uname' AND `pass` = '$pass'");
+    if(mysql_num_rows($sql)>0){
+        echo "you are now logged in.";
+        exit();
+    }else{
+        echo "Wrong U/P combination.";
+    }
+
+}else{
+
+    $form=<<<EOT
+<form action="login.php" method="POST">
+Username: <input type="text" name="uname" /><br />
+Password: <input type="password" name="pass" /><br />
+<input type="submit" name="submit" value="Log in" />
+</form>
+EOT;
+    echo $form;
+}
+
 
 ?>
 
